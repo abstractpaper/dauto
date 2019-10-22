67176ff999ec9f1c6bdbbd11ca2e6b0e667e9c73
abstractpaper <aziz.alfoudari@gmail.com>
2019-10-22 09:29:58 +0300

    Check pre-commit hook existance in TestHook

--- cmd/hook_test.go
+++ cmd/hook_test.go
@@ -1,11 +1,20 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
 	"testing"
 )
 
 func TestHook(t *testing.T) {
 	initRepo(t)
 	hook(git_tmp_path)
+
+	path := fmt.Sprintf("%s/.git/hooks/pre-commit", git_tmp_path)
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) || info.IsDir() {
+		t.Fatalf("pre-commit hook was not created at: %s", path)
+	}
+
 	deleteLocalRepo(t)
 }
